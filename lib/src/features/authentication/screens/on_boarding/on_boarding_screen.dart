89b58bb11c9796ca854968094aa6d4f1c08962bd
shishirlb <114382895+shishirlb@users.noreply.github.com>
2023-10-26 22:43:08 +0530

    Optimize imports

--- lib/src/features/authentication/screens/on_boarding/on_boarding_screen.dart
+++ lib/src/features/authentication/screens/on_boarding/on_boarding_screen.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_blog_app/src/constants/colors.dart';
-import 'package:flutter_blog_app/src/constants/image_strings.dart';
-import 'package:flutter_blog_app/src/constants/text_strings.dart';
 import 'package:flutter_blog_app/src/features/authentication/controllers/on_boarding_controller.dart';
-import 'package:flutter_blog_app/src/features/authentication/models/on_boarding.dart';
-import 'package:flutter_blog_app/src/features/authentication/screens/on_boarding/on_boarding_page_widget.dart';
 import 'package:get/get.dart';
 import 'package:liquid_swipe/liquid_swipe.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
